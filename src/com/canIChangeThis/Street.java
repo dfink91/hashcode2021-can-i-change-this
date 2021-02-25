82c9d7141abb4416df961a30dea928aa674daa23
Andrea Pellegrin <andreapellegrin@hotmail.it>
2021-02-25 19:06:59 +0100

    Street update

--- src/com/canIChangeThis/Street.java
+++ src/com/canIChangeThis/Street.java
@@ -3,5 +3,14 @@ package com.canIChangeThis;
 public class Street {
 
     public String name;
+    public String startIntersection;
+    public String endIntersection;
+    public int secondsToEnd;
 
+    public Street(String name, String startIntersection, String endIntersection, int secondsToEnd) {
+        this.name = name;
+        this.startIntersection = startIntersection;
+        this.endIntersection = endIntersection;
+        this.secondsToEnd = secondsToEnd;
+    }
 }
