b07bb392e32e46438f294c93cbe1e21d7970f1cc
Andrea Pellegrin <andreapellegrin@hotmail.it>
2021-02-25 19:21:06 +0100

    Intersection created

---
+++ src/com/canIChangeThis/Intersection.java
@@ -0,0 +1,14 @@
+package com.canIChangeThis;
+
+public class Intersection {
+
+    public int id;
+    public String intStreet;
+    public String outStreet;
+
+    public Intersection(int id, String intStreet, String outStreet) {
+        this.id = id;
+        this.intStreet = intStreet;
+        this.outStreet = outStreet;
+    }
+}
